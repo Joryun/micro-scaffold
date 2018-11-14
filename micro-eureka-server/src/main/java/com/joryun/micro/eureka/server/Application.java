6f155602754f799baf4bac400ca3f6ae03f3a984
Joryun <me@joryun.com>
2018-11-14 23:47:53 +0800

    feat: add two nodes eureka

--- micro-eureka-server/src/main/java/com/joryun/micro/eureka/server/Application.java
+++ micro-eureka-server/src/main/java/com/joryun/micro/eureka/server/Application.java
@@ -2,16 +2,16 @@ package com.joryun.micro.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author joryun
  */
 @SpringBootApplication
-@EnableDiscoveryClient
-public class MicroEurekaServerApplication {
+@EnableEurekaServer
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroEurekaServerApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
