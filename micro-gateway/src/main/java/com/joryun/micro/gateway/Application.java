41fd5323cad19a25a32fe6989f4c6078d3eb566c
Joryun <me@joryun.com>
2018-11-27 00:36:56 +0800

    feat: add gateway module

---
+++ micro-gateway/src/main/java/com/joryun/micro/gateway/Application.java
@@ -0,0 +1,19 @@
+package com.joryun.micro.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author joryun
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
